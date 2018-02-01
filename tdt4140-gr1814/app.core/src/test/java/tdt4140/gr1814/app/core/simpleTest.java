3bb10fe2a126f92ee4ed63242dac37a69b677514
soms2 <soms2@10.22.16.120>
2018-02-01 17:46:41 +0100

    La til testen

---
+++ tdt4140-gr1814/app.core/src/test/java/tdt4140/gr1814/app/core/simpleTest.java
@@ -0,0 +1,12 @@
+package tdt4140.gr1814.app.core;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class simpleTest {
+
+	@Test
+	public void TestExample() {
+		assertTrue(true);
+	}
+}
