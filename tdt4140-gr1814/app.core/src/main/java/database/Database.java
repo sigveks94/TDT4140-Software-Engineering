10c10693cd8a623c2fd5a8e14237d7bd9291386d
Harald Aarskog <haarskog@gmail.com>
2018-02-19 10:08:57 +0100

    Initiated a database class

---
+++ tdt4140-gr1814/app.core/src/main/java/database/Database.java
@@ -0,0 +1,10 @@
+package database;
+
+public class Database {
+	
+	//tar inn et personobjekt, henter ut informasjon, lagrer det i en fil
+	//skal ikke hente ut informasjon
+	//sjekk Gitlab for kommentarer til tilhørende issue
+	
+
+}
