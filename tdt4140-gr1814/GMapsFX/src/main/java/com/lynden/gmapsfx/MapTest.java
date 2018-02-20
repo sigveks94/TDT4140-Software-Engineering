a3dd8bce5925c863b2da109ac5b84543e66c824b
Svein Ole <Svein Ole@10.220.41.253>
2018-02-20 12:56:28 +0100

    Try

---
+++ tdt4140-gr1814/GMapsFX/src/main/java/com/lynden/gmapsfx/MapTest.java
@@ -0,0 +1,47 @@
+package com.lynden.gmapsfx;
+
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.shapes.Circle;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebEvent;
+import javafx.stage.Stage;
+
+public class MapTest extends Application implements MapComponentInitializedListener{
+
+	GoogleMapView GMap;
+	GoogleMap map;
+	
+	public static void main(String[] args) {
+		System.setProperty("java.net.useSystemProxies", "true");
+		launch(args);
+	}
+
+    @Override
+    public void mapInitialized() {
+        LatLong center = new LatLong(63.446827, 10.421906);
+        
+        MapOptions options = new MapOptions();
+        options.center(center).zoom(7);
+        map = GMap.createMap(options);  
+    }
+    
+	@Override
+	public void start(Stage arg0) throws Exception {
+		GMap = new GoogleMapView();
+		GMap.addMapInitializedListener(this);
+		GMap.setDisableDoubleClick(true);
+		GMap.getWebview().getEngine().setOnAlert((WebEvent<String> event) -> {});
+		
+		BorderPane p = new BorderPane();
+		p.setCenter(GMap);
+		
+		Scene scene = new Scene(p);
+		arg0.setScene(scene);
+		arg0.show();
+	}
+}
