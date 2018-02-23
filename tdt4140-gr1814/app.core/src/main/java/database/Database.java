7ffa5b105934e3b77301f43fe58354ac944e93c3
Harald Aarskog <haarskog@gmail.com>
2018-02-23 21:42:13 +0100

    Added XML-serialization.

--- tdt4140-gr1814/app.core/src/main/java/database/Database.java
+++ tdt4140-gr1814/app.core/src/main/java/database/Database.java
@@ -1,10 +1,26 @@
 package database;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
 public class Database {
 	
-	//tar inn et personobjekt, henter ut informasjon, lagrer det i en fil
-	//skal ikke hente ut informasjon
-	//sjekk Gitlab for kommentarer til tilhørende issue
+	//This class takes a person object as input and simply writes its toString to a file.
+	private String fileName = "personDB.txt";
 	
-
+	
+	public void writeToFile(Patient patient) throws FileNotFoundException, UnsupportedEncodingException {
+		try {
+		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("personDB.txt", true)));
+		    out.println(patient);
+		    out.close();
+		} catch (IOException e) {
+		}
+	}
 }
