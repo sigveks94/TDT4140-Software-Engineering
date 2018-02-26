ef7d8eac7f643fc428acd428473351b060876145
Harald Aarskog <haarskog@gmail.com>
2018-02-26 13:57:25 +0100

    Added patient method

--- tdt4140-gr1814/app.core/src/test/java/tdt4140/gr1814/app/core/DatabaseTest.java
+++ tdt4140-gr1814/app.core/src/test/java/tdt4140/gr1814/app/core/DatabaseTest.java
@@ -1,10 +1,20 @@
 package tdt4140.gr1814.app.core;
 
+import org.junit.Assert;
+import org.junit.Before;
+
 import junit.framework.TestCase;
 
 public class DatabaseTest extends TestCase{
 	
-	public void connectionTest() {
-		
+	Database db = new Database();
+	
+	@Before
+	public void setUp() {
+		db.connect();
+	}
+	public void insertTest() {
+	//	"INSERT INTO Patient(SSN, FirstName, LastName) VALUES (999999,'Mathias','Kroken');"
 	}
 	}
+	
