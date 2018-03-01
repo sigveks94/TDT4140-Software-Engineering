225093717e205869f44f8e826953ff931994140f
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2018-03-01 13:41:02 +0100

    Commented on the OnLocationChangedListener interface

--- tdt4140-gr1814/app.core/src/main/java/tdt4140/gr1814/app/core/OnLocationChangedListener.java
+++ tdt4140-gr1814/app.core/src/main/java/tdt4140/gr1814/app/core/OnLocationChangedListener.java
@@ -1,5 +1,7 @@
 package tdt4140.gr1814.app.core;
 
+//Functional interface which is supposed to be implemented in every class that would like to retrieve information whenever a object get its location changed.
+
 public interface OnLocationChangedListener {
 
 	public void onLocationChanged(String deviceId, Point newLocation);
