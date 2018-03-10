9b2ce740d12bfcd6ecdd112c2e6ca539f91e68fd
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2018-03-10 01:46:07 +0100

    Issue #32
    Though the webserver spits out errors left and right it runs. It
    responds to Http GET-request.

--- tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/Server.java
+++ tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/Server.java
@@ -1,5 +1,28 @@
 package tdt4140.gr1814.webserver;
 
-public class Server {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Server extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("Hello " + name);
+		writer.flush();
+		writer.close();
+	}
+	
 }
