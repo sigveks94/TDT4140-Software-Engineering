e30e903035dc659b1f55393ec35819ab4a0fc1d5
VemundFredriksen <vemund.fredriksen@hotmail.com>
2018-03-14 11:56:00 +0100

    Started a testcase for the loginservlet

---
+++ tdt4140-gr1814/tdt4140.gr1814.webserver/src/test/java/tdt4140/gr1814/webserver/LoginServletIT.java
@@ -0,0 +1,29 @@
+package tdt4140.gr1814.webserver;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class LoginServletIT{
+	
+	ConnectionHandler databaseConnection;
+	
+	@Before
+	public void setUp() {
+		databaseConnection = new ConnectionHandler();
+		databaseConnection.connect();
+	}
+	
+	@Test
+	public void loginFail() {
+		
+	}
+	
+	
+	@After
+	public void tearDown() {
+		databaseConnection = null;
+	}
+}
