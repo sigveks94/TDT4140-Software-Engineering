a022b783ebbdd2f9c53db62a339c22eb130029cb
VemundFredriksen <vemund.fredriksen@hotmail.com>
2018-03-14 11:58:35 +0100

    Issue #36
    It is now possible to log in through the webserver. The servlet does not
    pass back intuitive response codes though.

--- tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/LoginServlet.java
+++ tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/LoginServlet.java
@@ -1,21 +1,63 @@
 package tdt4140.gr1814.webserver;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import tdt4140.gr1814.app.core.Caretaker;
+
 public class LoginServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
+	ConnectionHandler databaseConnection;
 	
+	public LoginServlet() {
+		databaseConnection = new ConnectionHandler();
+		databaseConnection.connect();
+	}
 	
-	//The login should be done through a post request
+	/*
+	 * POST REQUEST for login
+	 * Expects username and password as parameter
+	 */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		
+		
+			String username = req.getParameter("username");
+			String password = req.getParameter("password");
+			
+			System.out.println("POST:" + username + ", pw:" + password);
+			
+			if(username == null || password == null) {
+				return; //Return errorcode here
+			}
+		
+			try {
+				ArrayList<ArrayList<String>> caretaker = databaseConnection.query("SELECT * FROM Caretaker WHERE Username ='"+username+"'");
+				if(caretaker.isEmpty()) {
+					return; // Return invalid login here
+				}
+				String pw = caretaker.get(0).get(1);
+				if(password.equals(password)) {
+					System.out.println("success");
+					PrintWriter writer = resp.getWriter();
+					writer.print("{\"username\":\"" + caretaker.get(0).get(0) + "\"}");
+					return; // Return success code
+				}
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 	}
 	
 	
