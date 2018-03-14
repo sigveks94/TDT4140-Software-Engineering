4a41b853ab2fb8cdc4fbf0e8c8f5b9b09aec31ef
sigveks <sigveks.stud@ntnu.no>
2018-03-14 12:39:03 +0100

    Push latest

--- tdt4140-gr1814/app.ui/src/main/java/tdt4140/gr1814/app/ui/MapViewController.java
+++ tdt4140-gr1814/app.ui/src/main/java/tdt4140/gr1814/app/ui/MapViewController.java
@@ -150,7 +150,6 @@ public class MapViewController implements Initializable, MapComponentInitialized
 		patientView();
 		myController.setScreen(ApplicationDemo.PatientOverviewID);
 	}
-	
 	public void zoneView(Patient currentPatient) {
 		this.currentPatient = currentPatient;
 		menu_btn.setVisible(false);
