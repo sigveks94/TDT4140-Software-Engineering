1d480b808146c1d869d36551dc7b92eb5c3191c6
VemundFredriksen <vemund.fredriksen@hotmail.com>
2018-03-14 11:05:55 +0100

    Deleted DatabaseTest because it will be obsolete soon. Created a LoginServlet backend

---
+++ tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/LoginServlet.java
@@ -0,0 +1,23 @@
+package tdt4140.gr1814.webserver;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	//The login should be done through a post request
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	
+
+}
