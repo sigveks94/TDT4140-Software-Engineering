8f2cb0b4f3ec9a3d10a0810f5e8018fbebc8dec1
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2018-03-14 18:26:05 +0100

    Issue #37
    Updated the class with response codes, comments and made a custom JSON
    serialization when zones associated with a caretaker_id is passed as
    argument

--- tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/ZoneServlet.java
+++ tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/ZoneServlet.java
@@ -12,17 +12,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
+//This is the sevlet supposed to handle all fetching and updating of zones in the database
 public class ZoneServlet extends HttpServlet{
 
 	//Serial Version, if the servlet ever changes something that will have inpact on the http request,
 	//the serial version should also be updated
 	private static final long serialVersionUID = 1L;
 	
+	//The connection helper class
 	ConnectionHandler databaseConnection;
 	
-	public ZoneServlet() {
+	//Privat method for establishing connection with the database
+	private void establishConnection(HttpServletResponse resp) {
 		databaseConnection = new ConnectionHandler();
-		databaseConnection.connect();
+		try {
+			databaseConnection.connect();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			resp.setStatus(500); //Internal DB error
+			return;
+		} 
 	}
 	
 	/*
@@ -30,9 +39,11 @@ public class ZoneServlet extends HttpServlet{
 	 * - patientId : returns the zone associated with the patient given
 	 * - caretaker_id : returns all zones associated with all patients associated with the given caretaker
 	 */
-	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//Establishes connection with the database
+		this.establishConnection(resp);
 		
 		//Fetches the response input stream in order to echo answer back to the requester
 		PrintWriter echoWriter = resp.getWriter();
@@ -40,52 +51,69 @@ public class ZoneServlet extends HttpServlet{
 		//If a SSN is passed, the response should be the zone associated with this patient
 		if(req.getParameter("ssn") != null) {
 			Long ssn = null;
-			try {
+			try { //Tries to parse to long, if parse failes the argument is considered invalid
 				ssn = Long.valueOf(req.getParameter("ssn"));
+				//Even though the parse works it does not prove this is a valid SSN, this checks that the given SSN is in fact 11 digits long
+				if(req.getParameter("ssb").length()!= 11) {
+					resp.setStatus(400); //Bad Request Code
+					return;
+				}
 			}
 			catch(NumberFormatException exception) {
 				exception.printStackTrace();
+				resp.setStatus(400); //Bad Request Code
 				return;
 			}
 			
+			//If all of the above works, the query is executed, the result is coded into a JSON serialization and the result is echoed back to the client that performed the HTTPRequest
 			echoWriter.print(toJson(getZone(ssn)));
 			echoWriter.flush();
 			echoWriter.close();
 		}
 		//If the given argument is a caretaker username
-		else if(req.getParameter("caretaker_id") != null){;
+		else if(req.getParameter("caretaker_id") != null){
+			//If the given caretakerId does not corrospond to any zones this request will simply return a empty list and therefor does not need any further validation unlike the alternativ above
 			echoWriter.print(toJson(getAllZones(req.getParameter("caretaker_id"))));
 			echoWriter.flush();
 			echoWriter.close();
 		}
+		else { //If none of the above options was satisified the input was on invalid form
+			resp.setStatus(400); //Bad Request code
+			return;
+		}
 		
 	}
 	
+	//Private method for getting zones associated with a single patient given the SSN of the patient
 	private ArrayList<ArrayList<String>> getZone(long ssn){
 		try {
 			return databaseConnection.query("SELECT * FROM ZonePoint INNER JOIN Zone ON Zone.ZoneID = ZonePoint.ZoneID WHERE Zone.PatientSSN LIKE '" + ssn + "'");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
+	//Private method that fetches all zones associated with all the patients assoiated with a given care taker given by the care taker username
 	private ArrayList<ArrayList<String>> getAllZones(String caretakerId){
 		try {
 			return databaseConnection.query("SELECT Zone.patientSSN, ZonePoint.*, PatientCaretaker.CaretakerUsername FROM ZonePoint NATURAL JOIN Zone "
 					+ "INNER JOIN PatientCaretaker ON Zone.PatientSSN = PatientCaretaker.PatSSN WHERE PatientCaretaker.CaretakerUsername = '" + caretakerId + "'");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
-	private String toJson(Object o) {
-		Gson jsonParser = new Gson();
-		return jsonParser.toJson(o);
+	//Takes an ArrayList holding an ArrayList of strings as paramter. This should essentially cosist of a list of zonecursors from the SQL Query.
+	//This method parses the list into a JSON serialized String on the format: {{"zone_id":1,"ssn":12345678901,"lat":63.41949000273954,"long":10.397296119049088,"point_order": 5},{ --==--}}
+	private String toJson(ArrayList<ArrayList<String>> result) {
+		
+		String json = "{";
+		for(ArrayList<String> lst: result) {
+			json += "{\"zone_id\":" + lst.get(5) + ",\"ssn\":" + lst.get(0) + ",\"lat\":" + lst.get(2) + ",\"long\":" + lst.get(3) + ",\"point_order\":" + lst.get(4) + "},";
+		}
+		
+		return json.substring(0, json.length()-1) + "}";
 	}
-	
-	
 }
