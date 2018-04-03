43187dfe6c0a08b4a56a2d4130b726dde7961885
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2018-04-03 17:31:39 +0200

    Issue #46. Created a new connection handler that will not use the old way of getting data from a index in a array, but rather just return the result set because the method that called for the result set knows better how to treat the result.

---
+++ tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/DatabaseHandler.java
@@ -0,0 +1,25 @@
+package tdt4140.gr1814.webserver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//The database handler handles all traffic between the web server and the database
+public class DatabaseHandler {
+	
+	private Connection connection;
+	
+	public DatabaseHandler() throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		connection = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/hara_database?autoReconnect=true&useSSL=false","hara_db","gruppe14");
+	}
+	
+	public ResultSet query(String query) throws SQLException {
+		Statement statement = this.connection.createStatement();
+		statement.executeQuery(query);
+		return statement.getResultSet();
+	}
+
+}
