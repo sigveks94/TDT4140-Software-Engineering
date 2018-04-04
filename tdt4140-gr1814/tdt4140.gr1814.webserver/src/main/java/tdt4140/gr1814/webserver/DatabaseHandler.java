0925cbe848a109b435feb9062012e0c549babbf1
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2018-04-04 16:01:08 +0200

    Issue #46
    Did better commenting on the DatabaseHandler class

--- tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/DatabaseHandler.java
+++ tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/DatabaseHandler.java
@@ -6,9 +6,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-//The database handler handles all traffic between the web server and the database
+/*
+ * This class will handle all traffic between the web server and the database
+ * All methods throws SQLException, if a method does not throw an exception the query is interpreted as successfull
+ * The methods throws exceptions because the method calling knows better how to handle the exception
+ */
 public class DatabaseHandler {
 	
+	//The connection to the database is set up on instantiation
 	private Connection connection;
 	
 	public DatabaseHandler() throws SQLException, ClassNotFoundException {
@@ -16,12 +21,14 @@ public class DatabaseHandler {
 		connection = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/hara_database?autoReconnect=true&useSSL=false","hara_db","gruppe14");
 	}
 	
+	//Method for handling queries that will return a result
 	public ResultSet query(String query) throws SQLException {
 		Statement statement = this.connection.createStatement();
 		statement.executeQuery(query);
 		return statement.getResultSet();
 	}
 	
+	//Method for handling queries that will do update on a table, no result returned
 	public void update(String query) throws SQLException{
 		Statement statement = this.connection.createStatement();
 		statement.executeUpdate(query);
