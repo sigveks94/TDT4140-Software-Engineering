35f53b74264b733d5d9b23b97f03dd27cd8705a7
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2018-04-04 15:01:19 +0200

    Issue #46
    The CareTaker Servlet is now using the new database handler and is
    considered done.

--- tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/servlet/CaretakerServlet.java
+++ tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/servlet/CaretakerServlet.java
@@ -1,84 +1,89 @@
 package tdt4140.gr1814.webserver.servlet;
 
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import tdt4140.gr1814.webserver.DatabaseHandler;
 
-import tdt4140.gr1814.webserver.ConnectionHandler;
+/*
+ * ================== RESPONSE CODES ===================
+ * 			200 - OK
+ * 			400 - Bad Arguments
+ * 			500 - Internal Database Error, not users fault
+ */
+
 
 public class CaretakerServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 	
-	ConnectionHandler databaseConnection = null;
-	
-	//Private method for establishing connection with the database
-	private boolean establishConnection(HttpServletResponse resp) {
-		databaseConnection = new ConnectionHandler();
-		try {
-			databaseConnection.connect();
-			return true;
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-			resp.setStatus(500); //Internal DB error
-			return false;
-		} 
-	}
-	
 	/*
 	 *	POST REQUEST can handle the follow:
 	 *	- Change of password given username and new password
 	 */
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		//Sets up the connection to the database
-		if(!this.establishConnection(resp)) {
-			resp.setStatus(500); //Internal DB Error
+		String userName = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		//If either the username og password arguments are missing the request is bad
+		if(userName == null || password == null) {
+			resp.setStatus(400);
 			return;
 		}
 		
-		//This is the expected paramaters for a password change
-		if(req.getParameter("username") != null && req.getParameter("password") != null) {
-			if(this.changePassword(req.getParameter("username"), req.getParameter("password"))) {
-				resp.setStatus(200);
-				return;
-			}
-			else {
-				resp.setStatus(500); //Internal DB Error
-				return;
-			}
-		}
+		this.changePassword(userName, password, resp);
 		
 		
 	}
 	
 	//Private method for handling a change of password
-	private boolean changePassword(String caretakerUsername, String newPassword) {
+	private void changePassword(String caretakerUsername, String newPassword, HttpServletResponse resp) {
+		
+		DatabaseHandler databaseHandler;
+		
+		try {
+			databaseHandler = new DatabaseHandler();
+		} catch (ClassNotFoundException | SQLException e1) {
+			e1.printStackTrace();
+			resp.setStatus(500);
+			return;
+		}
 		
 		String query = "SELECT Username FROM Caretaker WHERE Username LIKE \"" + caretakerUsername + "\"";
-		ArrayList<ArrayList<String>> resultList = null;
+		ResultSet result;
 		try {
-			resultList = databaseConnection.query(query);
-			if(resultList.size() != 1)
-				return false;
+			result = databaseHandler.query(query);
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return false;
+			return;
+		}
+		try {
+			if(!result.next()) {
+				resp.setStatus(400);
+				return;
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			resp.setStatus(500);
+			return;
 		}
 		
 		query = "UPDATE Caretaker SET Password = \"" + newPassword + "\" WHERE Username LIKE \"" + caretakerUsername + "\";";
 		try {
-			databaseConnection.update(query);
-			return true;
+			databaseHandler.update(query);
+			resp.setStatus(200);
+			return;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return false;
+			resp.setStatus(500);
+			return;
 		}
 		
 		
