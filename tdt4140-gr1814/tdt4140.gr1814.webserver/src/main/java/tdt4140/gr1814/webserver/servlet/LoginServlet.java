4c8869f4a22919ff0fe0162d61bbb6f74b3e5b46
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2018-04-04 16:03:27 +0200

    Issue #46
    Added commenting to the loginServlet

--- tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/servlet/LoginServlet.java
+++ tdt4140-gr1814/tdt4140.gr1814.webserver/src/main/java/tdt4140/gr1814/webserver/servlet/LoginServlet.java
@@ -10,6 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import tdt4140.gr1814.webserver.DatabaseHandler;
 
 /*
+ * This servlet simply handles log in requests
+ * If a log in is considered succesful it will echo back information regarding the care taker that logged in
+ * 
+ */
+
+/*
  * ================== RESPONSE CODES ===================
  * 			200 - OK
  * 			400 - Bad Arguments
@@ -24,6 +30,7 @@ public class LoginServlet extends HttpServlet{
 	 * POST REQUEST for login
 	 * Expects username and password as parameter
 	 */
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			
